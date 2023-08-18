d1703ea3f92aa96e17d7206fa3faf9ee68daeabb
ncqwer <1732031236@qq.com>
2023-08-18 19:20:39 +0800

    doc: 关于tree的一些概念更新

--- playground/src/pages/size.tsx
+++ playground/src/pages/size.tsx
@@ -1,6 +1,27 @@
 import { useCollapseStyles } from '@lla-ui/utils';
 import React from 'react';
 
+const Parent = ({ children }) => {
+  React.useEffect(() => {
+    console.log(
+      '%c [ Parent ]-5',
+      'font-size:13px; background:pink; color:#bf2c9f;',
+    );
+  });
+  return children;
+};
+
+const Child = ({ hsjKey }) => {
+  React.useEffect(() => {
+    console.log(
+      '%c [ Child ]-5',
+      'font-size:13px; background:pink; color:#bf2c9f;',
+      hsjKey,
+    );
+  });
+  return null;
+};
+
 export default () => {
   const [open, setOpen] = React.useState(false);
 
@@ -10,10 +31,14 @@ export default () => {
   });
   return (
     <div>
+      <Parent>
+        <Child hsjKey={1}></Child>
+        <Child hsjKey={2}></Child>
+      </Parent>
       <button onClick={() => setOpen((prev) => !prev)}>toggle</button>
       <div style={styles}>
         {isMount && (
-          <div ref={ref} contentEditable>
+          <div ref={ref}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
             neque velit tenetur ducimus quaerat necessitatibus dolores assumenda
             sint quod adipisci corporis consectetur, veritatis qui quasi,
